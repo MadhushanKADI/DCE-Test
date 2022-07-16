5818504674aa33eaacbe261e693c8e6826041b2a
Madhushan <isurumadushanmail@gmail.com>
2022-07-16 16:12:44 +0530

    add defalt router path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { UserIndexComponent } from './user-index/user-index.component';
 import { UserRegComponent } from './user-reg/user-reg.component';
 
 const routes: Routes = [
+  { path: '', component: UserIndexComponent},
   { path: 'user-index', component: UserIndexComponent},
   { path: 'user-reg', component: UserRegComponent}
 ];
